b0797d00fd4c1666d29b0ea8ecca164c5d9895e0
Maksim <max.14070@yandex.ru>
2023-02-28 01:01:08 +0300

    com2

--- app/src/main/java/com/example/pr2/MainActivity.java
+++ app/src/main/java/com/example/pr2/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.pr2;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,24 +22,49 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MyApp";
 
+    ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+
+                    CheckBox checkbox_1 = findViewById(R.id.checkbox_1);
+                    CheckBox checkbox_2 = findViewById(R.id.checkbox_2);
+
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent intent = result.getData();
+                        boolean but1 = intent.getBooleanExtra("start_1", false);
+                        boolean but2 = intent.getBooleanExtra("start_2", false);
+
+                        checkbox_1.setChecked(but1);
+                        checkbox_2.setChecked(but2);
+
+                    }
+                }
+            });
+
+
+
     @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        ImageView imageCake = findViewById(R.id.imageCake);
-        TextView main_text = findViewById(R.id.main_text);
+
 
         CheckBox checkbox_1 = findViewById(R.id.checkbox_1);
         CheckBox checkbox_2 = findViewById(R.id.checkbox_2);
 
-        Button button_search = findViewById(R.id.button_search);
+
         Button button_next_page = findViewById(R.id.button_next_page);
 
-        main_text.setText(R.string.main_text);   // задание 3
+        ImageView imageCake = findViewById(R.id.imageCake);  // задание 3
+        TextView main_text = findViewById(R.id.main_text);
+        main_text.setText(R.string.main_text);
         imageCake.setImageResource(R.drawable.apple);   // задание 3
 
+        Button button_search = findViewById(R.id.button_search);
         button_search.setOnClickListener(new View.OnClickListener() {   // задание 4
             @Override
             public void onClick(View v) {
@@ -42,6 +73,18 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        button_next_page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean ch1 = checkbox_1.isChecked();
+                boolean ch2 = checkbox_2.isChecked();
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("check1", ch1);
+                intent.putExtra("check2", ch2);
+                mStartForResult.launch(intent);
+            }
+        });
     }
 
     public void btnOneClick(View v)  // задание 4 - вариант 2
---
+++ app/src/main/java/com/example/pr2/SecondActivity.java
@@ -0,0 +1,41 @@
+package com.example.pr2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Bundle arg = getIntent().getExtras();
+        boolean ch1 = (boolean) arg.get("check1");
+        boolean ch2 = (boolean) arg.get("check2");
+
+        CheckBox chbx1 = findViewById(R.id.checkBox1_sec_page);
+        CheckBox chbx2 = findViewById(R.id.checkBox2_sec_page);
+
+        chbx1.setChecked(ch1);
+        chbx2.setChecked(ch2);
+
+        Button bt = findViewById(R.id.button_next_page2);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent();
+                data.putExtra("start_1", ch1);
+                data.putExtra("start_2", ch2);
+                setResult(RESULT_OK, data);
+                finish();
+            }
+        });
+
+    }
+}
